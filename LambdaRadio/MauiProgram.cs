8f87415424ceb81d9139828ed03b4b961ec2dc14
Benjamin Vera <contact@benjaminvera.com>
2023-05-06 22:01:51 -0500

    adding native audio service

--- LambdaRadio/MauiProgram.cs
+++ LambdaRadio/MauiProgram.cs
@@ -1,39 +1,52 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
 
 namespace LambdaRadio;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("Spotify-Bold.woff2", "SpotifyBold");
-				fonts.AddFont("Spotify-Light", "SpotifyLight");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("Spotify-Bold.woff2", "SpotifyBold");
+                fonts.AddFont("Spotify-Light", "SpotifyLight");
+            });
 
-		/*
+        /*
 			Before we can use this ViewModel in a View,
 			we need to register it with the service collection in MauiProgram
 			so that we can inject it into the View’s constructor. 
 		 */
 
-		builder.Services.AddTransient<HomePage>();
-		builder.Services.AddTransient<SearchPage>();
-		builder.Services.AddTransient<FavoritesPage>();
+        builder.Services.AddTransient<HomePage>();
+        builder.Services.AddTransient<SearchPage>();
+        builder.Services.AddTransient<FavoritesPage>();
 
-		builder.Services.AddTransient<HomeViewModel>();
-		builder.Services.AddTransient<SearchViewModel>();
-		builder.Services.AddTransient<FavoritesViewModel>();
+        builder.Services.AddTransient<HomeViewModel>();
+        builder.Services.AddTransient<SearchViewModel>();
+        builder.Services.AddTransient<FavoritesViewModel>();
+
+
+#if WINDOWS
+        builder.Services.TryAddSingleton<SharedMauiLib.INativeAudioService, SharedMauiLib.Platforms.Windows.NativeAudioService>();
+#elif ANDROID
+        builder.Services.TryAddSingleton<SharedMauiLib.INativeAudioService, SharedMauiLib.Platforms.Android.NativeAudioService>();
+#elif MACCATALYST
+        builder.Services.TryAddSingleton<SharedMauiLib.INativeAudioService, SharedMauiLib.Platforms.MacCatalyst.NativeAudioService>();
+#elif IOS
+        builder.Services.TryAddSingleton<SharedMauiLib.INativeAudioService, SharedMauiLib.Platforms.iOS.NativeAudioService>();
+#endif
+
 
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
 
